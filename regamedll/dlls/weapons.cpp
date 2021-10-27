79b630ff79093e04f1eba8ea0c997a0429722b51
StevenKal <release@amxmod.net>
2021-10-27 05:45:51 +0200

    Update weapons.cpp

--- regamedll/dlls/weapons.cpp
+++ regamedll/dlls/weapons.cpp
@@ -1401,7 +1401,7 @@ BOOL EXT_FUNC CBasePlayerWeapon::__API_HOOK(DefaultDeploy)(char *szViewModel, ch
 	m_pPlayer->m_iLastZoom = DEFAULT_FOV;
 	m_pPlayer->m_bResumeZoom = false;
 
-	if(m_pPlayer->HasShield() && m_iId != WEAPON_C4 && m_pPlayer->pev->weaponmodel)
+	if(m_pPlayer->HasShield() && m_iId != WEAPON_C4/* && m_pPlayer->pev->viewmodel && m_pPlayer->pev->weaponmodel*/)
 	{
 		m_pPlayer->pev->gamestate = HITGROUP_SHIELD_ENABLED;
 	}
