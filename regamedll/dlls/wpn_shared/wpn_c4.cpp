d9514989957e99540ebca64f063a9205e7caa113
StevenKal <release@amxmod.net>
2021-11-04 04:58:35 +0100

    Update wpn_c4.cpp

--- regamedll/dlls/wpn_shared/wpn_c4.cpp
+++ regamedll/dlls/wpn_shared/wpn_c4.cpp
@@ -312,7 +312,16 @@ void CC4::WeaponIdle()
 	{
 		if (m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
 		{
-			RetireWeapon();
+#ifdef REGAMEDLL_API
+			if ((m_pPlayer->pev->weapons & ~(1 << WEAPON_SUIT | 1 << m_iId )) == 0)
+			{
+				m_pPlayer->CSPlayer()->RemovePlayerItem(STRING(pev->classname));
+			}
+			else
+#endif
+			{
+				RetireWeapon();
+			}
 			return;
 		}
 
