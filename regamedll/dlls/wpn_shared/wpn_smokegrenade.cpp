c84f926bbd2f10fda5d204f8a41d26a53496d5bb
StevenKal <release@amxmod.net>
2021-11-04 13:28:22 +0100

    Update wpn_smokegrenade.cpp

--- regamedll/dlls/wpn_shared/wpn_smokegrenade.cpp
+++ regamedll/dlls/wpn_shared/wpn_smokegrenade.cpp
@@ -237,11 +237,13 @@ void CSmokeGrenade::WeaponIdle()
 		}
 		else
 		{
+#ifdef REGAMEDLL_FIXES
 			if ((m_pPlayer->pev->weapons & ~(1 << WEAPON_SUIT | 1 << m_iId )) == 0)
 			{
 				Holster();
 			}
 			else
+#endif
 			{
 				RetireWeapon();
 			}
