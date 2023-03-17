bf20923b81d159c2aa97b5d3519ef8aaef43a3cf
StevenKal <release@amxmod.net>
2023-03-17 11:10:21 +0000

    Update player.cpp

--- regamedll/dlls/player.cpp
+++ regamedll/dlls/player.cpp
@@ -4045,7 +4045,12 @@ void CBasePlayer::PlayerUse()
 			CBaseEntity *pTrain = Instance(pev->groundentity);
 			if (pTrain && pTrain->Classify() == CLASS_VEHICLE)
 			{
+#ifdef REGAMEDLL_ADD
+				if (legacy_vehicle_block.value)
+					((CFuncVehicle *)pTrain)->m_pDriver = nullptr;
+#else
 				((CFuncVehicle *)pTrain)->m_pDriver = nullptr;
+#endif
 			}
 			return;
 		}
@@ -4596,7 +4601,12 @@ void EXT_FUNC CBasePlayer::__API_HOOK(PreThink)()
 			// Turn off the train if you jump, strafe, or the train controls go dead
 			m_afPhysicsFlags &= ~PFLAG_ONTRAIN;
 			m_iTrain = (TRAIN_NEW | TRAIN_OFF);
+#ifdef REGAMEDLL_ADD
+			if (legacy_vehicle_block.value)
+				((CFuncVehicle *)pTrain)->m_pDriver = nullptr;
+#else
 			((CFuncVehicle *)pTrain)->m_pDriver = nullptr;
+#endif
 			return;
 		}
 
