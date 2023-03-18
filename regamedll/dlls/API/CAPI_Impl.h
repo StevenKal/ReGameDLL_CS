72b50df57680fb2694e2a0cdcf46b345f7c61f9e
StevenKal <release@amxmod.net>
2023-03-18 10:51:22 +0000

    Update CAPI_Impl.h

--- regamedll/dlls/API/CAPI_Impl.h
+++ regamedll/dlls/API/CAPI_Impl.h
@@ -641,6 +641,10 @@ typedef IHookChainRegistryClassImpl<void, CBasePlayer> CReGameHookRegistry_CBase
 typedef IHookChainClassImpl<void, CBasePlayer> CReGameHook_CBasePlayer_JoiningThink;
 typedef IHookChainRegistryClassImpl<void, CBasePlayer> CReGameHookRegistry_CBasePlayer_JoiningThink;
 
+// FreeGameRules hook
+typedef IHookChainImpl<void, CGameRules **> CReGameHook_FreeGameRules;
+typedef IHookChainRegistryImpl<void, CGameRules **> CReGameHookRegistry_FreeGameRules;
+
 class CReGameHookchains: public IReGameHookchains {
 public:
 	// CBasePlayer virtual
@@ -774,6 +778,8 @@ public:
 	CReGameHookRegistry_CBasePlayer_DeathSound m_CBasePlayer_DeathSound;
 	CReGameHookRegistry_CBasePlayer_JoiningThink m_CBasePlayer_JoiningThink;
 
+	CReGameHookRegistry_FreeGameRules m_FreeGameRules;
+
 public:
 	virtual IReGameHookRegistry_CBasePlayer_Spawn *CBasePlayer_Spawn();
 	virtual IReGameHookRegistry_CBasePlayer_Precache *CBasePlayer_Precache();
@@ -904,6 +910,8 @@ public:
 	virtual IReGameHookRegistry_CBasePlayer_Pain *CBasePlayer_Pain();
 	virtual IReGameHookRegistry_CBasePlayer_DeathSound *CBasePlayer_DeathSound();
 	virtual IReGameHookRegistry_CBasePlayer_JoiningThink *CBasePlayer_JoiningThink();
+
+	virtual IReGameHookRegistry_FreeGameRules *FreeGameRules();
 };
 
 C_DLLEXPORT CReGameHookchains g_ReGameHookchains;
