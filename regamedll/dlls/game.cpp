d51e440374b28b826161ce6682c8019aac172027
StevenKal <release@amxmod.net>
2023-07-24 12:37:26 +0200

    New CVar: mp_dying_time

--- regamedll/dlls/game.cpp
+++ regamedll/dlls/game.cpp
@@ -168,6 +168,7 @@ cvar_t plant_c4_anywhere                 = { "mp_plant_c4_anywhere", "0", 0, 0.0
 cvar_t give_c4_frags                     = { "mp_give_c4_frags", "3", 0, 3.0f, nullptr };
 cvar_t hostages_rescued_ratio            = { "mp_hostages_rescued_ratio", "1.0", 0, 1.0f, nullptr };
 cvar_t legacy_vehicle_block              = { "mp_legacy_vehicle_block", "1", 0, 0.0f, nullptr };
+cvar_t dying_time                        = { "mp_dying_time", "3.0", 0, 3.0f, nullptr };
 
 // Note: Just for my plugins & cie.
 cvar_t game_version_personnal            = { "game_version_personnal", "1.3.0-public-AMXModDev", FCVAR_SERVER, 0.0f, nullptr };
@@ -418,6 +419,7 @@ void EXT_FUNC GameDLLInit()
 	CVAR_REGISTER(&give_c4_frags);
 	CVAR_REGISTER(&hostages_rescued_ratio);
 	CVAR_REGISTER(&legacy_vehicle_block);
+	CVAR_REGISTER(&dying_time);
 
 	CVAR_REGISTER(&game_version_personnal);
 
